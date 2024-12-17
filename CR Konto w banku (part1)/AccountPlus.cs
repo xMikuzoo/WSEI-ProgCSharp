2892e44389ec62b848e970b34dac181bbd75d4d6
Wojciech Król <wojciech.krol@adminbrothers.com>
2024-12-17 10:44:24 +0100

    cos tam dziala

---
+++ CR Konto w banku (part1)/AccountPlus.cs
@@ -0,0 +1,59 @@
+﻿namespace CR_Konto_w_banku__part1_
+{
+    public class AccountPlus : Account, IAccountWithLimit
+    {
+        private decimal _oneTimeDebetLimit;
+        public AccountPlus(string name, decimal initialBalance = 0, decimal initialLimit = 100):base(name, initialBalance) 
+        {
+            OneTimeDebetLimit = initialLimit;
+        }
+
+        public decimal OneTimeDebetLimit
+        {
+            get { return _oneTimeDebetLimit; }
+            set
+            {
+                if (!IsBlocked)
+                {
+                    _oneTimeDebetLimit = PrepareOneTimeDebetLimit(value);
+                }
+            }
+        }
+        public decimal AvaibleFounds { get { return Math.Round(Balance + OneTimeDebetLimit, PRECISION); } }
+
+        public new bool Withdrawal(decimal amount)
+        {
+            if (amount <= 0 || IsBlocked)
+            {
+                return false;
+            }
+            if (amount > AvaibleFounds)
+            {
+                return false;
+            }
+
+            if (amount <= Balance)
+            {
+                return base.Withdrawal(amount);
+            }
+
+            base.Withdrawal(Balance);
+            Block();
+            
+            return true;
+        }
+
+        private decimal PrepareOneTimeDebetLimit(decimal debetLimit)
+        {
+            if(debetLimit < 0m)
+            {
+                return 0m;
+            }
+            return Math.Round(debetLimit, PRECISION);
+        }
+
+        public override string ToString() =>
+        IsBlocked ? $"Account name: {Name}, balance: {Balance:F2}, blocked, avaible founds: {AvaibleFounds:F2}, limit: {OneTimeDebetLimit:F2}"
+                    : $"Account name: {Name}, balance: {Balance:F2}, avaible founds: {AvaibleFounds:F2}, limit: {OneTimeDebetLimit:F2}";
+    }
+}
--- CR Konto w banku (part1)/Program.cs
+++ CR Konto w banku (part1)/Program.cs
@@ -6,6 +6,34 @@
         {
 
 
+            // scenariusz: wpłaty wypłaty, blokada konta
+            // utworzenie konta plus z domyslnym limitem 100
+            var john = new AccountPlus("John", initialBalance: 100.0m);
+            Console.WriteLine(john);
+
+            // wypłata - podanie kwoty ujemnej
+            john.Withdrawal(-50.0m);
+            Console.WriteLine(john);
+
+            // wypłata bez wykorzystania debetu
+            john.Withdrawal(50.0m);
+            Console.WriteLine(john);
+
+            // wypłata z wykorzystaniem debetu
+            john.Withdrawal(100.0m);
+            Console.WriteLine(john);
+
+            // konto zablokowane, wypłata niemożliwa
+            john.Withdrawal(10.0m);
+            Console.WriteLine(john);
+
+            // wpłata odblokowująca konto
+            john.Deposit(80.0m);
+            Console.WriteLine(john);
+
+            // wpłata podanie kwoty ujemnej
+            john.Deposit(-80.0m);
+            Console.WriteLine(john);
         }
     }
 }
