7a46d0cc87f310c5e70e080e9bc21fb78da97d8f
Wojciech Król <wojciech.krol@adminbrothers.com>
2024-12-17 08:28:53 +0100

    init CR Konto w banku (part1)

---
+++ CR Konto w banku (part1)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CR_Konto_w_banku__part1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
